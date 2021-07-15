17b17561035906ec74c970077e6a9d07d43a7b4f
Henrique Fingler <hfingler@cs.utexas.edu>
2021-07-15 18:54:09 +0000

    more features

--- worker/serverless_gpu/memory_server/gpuserver.cpp
+++ worker/serverless_gpu/memory_server/gpuserver.cpp
@@ -1,6 +1,7 @@
 #include "gpuserver.hpp"
 
 #include <map>
+#include <memory>
 #include <string>
 #include <stdint.h>
 #include <stdlib.h>
@@ -22,8 +23,6 @@ namespace GPUMemoryServer {
 //when we need to enable peer access
 //https://github.com/NVIDIA/cuda-samples/blob/master/Samples/simpleIPC/simpleIPC.cu
 
-//TODO: we need to check if cudaMallocManaged works with cudaIpcGetMemHandle 
-
 void Server::run() {
     //go to our gpu
     checkCudaErrors( cudaSetDevice(gpu) );
@@ -32,25 +31,33 @@ void Server::run() {
     int client_sock = get_connection(sock);
     printf("Memory server of GPU %d ready\n", gpu);
 
-    //TODO: we need to keep a map of all allocations we did
-    std::map<uint64_t, Allocation> allocs;
-
     char* buffer = new char[BUF_SIZE];
     Request req;
     Reply reply;
     while(1) {
         int len = recv_message(sock, buffer, BUF_SIZE);
-
         //TODO: better (de)serialization
         memcpy(&req, buffer, sizeof(Request));
 
+        /*************************
+         *   cudaMalloc request
+         *************************/
         if (req.type == RequestType::ALLOC) {
+            //allocate and get the ipc handle
             void *ptr;
-            checkCudaErrors( cudaMalloc(&ptr, req.size) );
 
+            //TODO: if more than requested, use cudaMallocManaged
+            //TODO: we need to check if cudaMallocManaged works with cudaIpcGetMemHandle 
+            checkCudaErrors( cudaMalloc(&ptr, req.data.size) );
             cudaIpcMemHandle_t memHandle;
             checkCudaErrors( cudaIpcGetMemHandle(&memHandle, ptr) );
-            //TODO: save handle on a map, so we can cudaIpcCloseMemHandle when it's freed
+
+            //store on our metadata maps
+            uint64_t key = (uint64_t) ptr;
+            allocs[req.worker_id][key] = std::make_unique<Allocation>(req.data.size, ptr);
+
+            //c++ wizardy:  https://stackoverflow.com/questions/2667355/mapint-int-default-values
+            used_memory[req.worker_id] += req.data.size;
 
             //construct reply
             memcpy(&reply.memHandle, &memHandle, sizeof(cudaIpcMemHandle_t));
@@ -58,10 +65,37 @@ void Server::run() {
             //reply
             send_message(client_sock, buffer, sizeof(Reply));
         }
+        /*************************
+         *       cudaFree
+         *************************/
         else if (req.type == RequestType::FREE) {
-            //TODO
+            uint64_t ptr = (uint64_t) req.data.devPtr;
+            auto alloc = allocs[req.worker_id].find(ptr);
+
+            //not found, illegal free
+            if (alloc == allocs[req.worker_id].end()) {
+                printf("Worker %d tried to free %p, which wasn't allocated\n", req.worker_id, req.data.devPtr);
+            }
+            else {
+                //update used memory
+                used_memory[req.worker_id] -= alloc->second->size;
+                //this frees the handle and memory
+                allocs[req.worker_id].erase(alloc);
+            }
+        }
+        /*************************
+         * worker finished, cleanup
+         *************************/
+        else if (req.type == RequestType::FINISHED) {
+            used_memory[req.worker_id] = 0;
+            allocs[req.worker_id].clear();
         }
     }
+}
+
+Allocation::~Allocation() {
+    checkCudaErrors( cudaIpcCloseMemHandle(devPtr) );  //TODO: double check if we are opening for all requests
+    checkCudaErrors( cudaFree(devPtr) );
 }
 
 //end of namespace
