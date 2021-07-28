d997d71d82e67ae912b85b9f71572eb96e93d394
Zhiting Zhu <zhitingz@cs.utexas.edu>
2021-07-28 07:04:56 +0000

    add exposed
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- cava/samples/onnxruntime/onnx_dump.cpp
+++ cava/samples/onnxruntime/onnx_dump.cpp
@@ -791,7 +791,7 @@ CUresult __internal_cuLaunchKernel(CUfunction f, unsigned int gridDimX, unsigned
 }
 
 ava_begin_replacement;
-CUresult CUDAAPI cuLaunchKernel(CUfunction f, unsigned int gridDimX, unsigned int gridDimY, unsigned int gridDimZ,
+EXPORTED CUresult CUDAAPI cuLaunchKernel(CUfunction f, unsigned int gridDimX, unsigned int gridDimY, unsigned int gridDimZ,
                                 unsigned int blockDimX, unsigned int blockDimY, unsigned int blockDimZ,
                                 unsigned int sharedMemBytes, CUstream hStream, void **kernelParams, void **extra) {
   return __internal_cuLaunchKernel(f, gridDimX, gridDimY, gridDimZ, blockDimX, blockDimY, blockDimZ,
